655bb2dee6aeb04b1f173317a8a71573a4335cff
Anish <anishverma09072004@gmail.com>
2025-07-11 00:35:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./Pages/Home";
-import { NotFound } from "./Pages/NotFound";
-import { Toaster } from "@/components/ui/toaster"; // ✅ make sure this is correct
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <>
-      <Toaster /> {/* ✅ Mount this globally */}
+      <Toaster />
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
